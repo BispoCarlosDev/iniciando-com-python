bbaf1301fa82be051bd5ea37660cb98567eb644f
Carlos <carlos.oliveirabispo@hotmail.com>
2024-12-02 22:24:03 -0400

    finalizando os exercicios

--- oo-sabor-express/app.py
+++ oo-sabor-express/app.py
@@ -1,9 +1,11 @@
 from modelos.restaurante import Restaurante
 
 restaurante_praca = Restaurante('praça', 'Gourmet', '40028922', 'Rua Maringá')
+restaurante_japa = Restaurante('japa', 'japonesa', '40028922', 'Rua Maringá')
+restaurante_mexicano = Restaurante('mexicano', 'mexicana', '40028922', 'Rua Maringá')
 
 restaurante_praca.receber_avaliacao('Carlos', 9)
-restaurante_praca.receber_avaliacao('Pedro', 6)
+restaurante_praca.receber_avaliacao('Pedro', 3)
 restaurante_praca.receber_avaliacao('João', 5)
 
 def main():
